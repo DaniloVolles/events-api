cf6fe75bd7b3538b77303932d5b17ec7fcb0ea69
Danilo Volles <danilo.volles@outlook.com>
2026-03-03 23:57:21 -0300

    Initial commit

---
+++ src/test/java/br/com/danilovolles/events/api/EventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danilovolles.events.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
